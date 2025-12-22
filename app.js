c3be363fa66fe67a196779bc9307a2feb93db445
Daria <qqdashkan@icloud.com>
2025-12-22 15:28:55 +0200

    __

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { task_01 } from './task_01.js';
 
 // console.log(await task_01(path.join('csv_files', 'sci_fi_1.csv')));
 
-console.log(await task_02(path.join('csv_files', 'sci_fi_2.csv')));
+// console.log(await task_02(path.join('csv_files', 'sci_fi_2.csv')));
 
 // console.log(await task_03(path.join('csv_files', 'sci_fi_3.csv')));
 
